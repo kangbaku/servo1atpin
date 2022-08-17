80487b4287e5c9c58e33445d0e168b660b0438e3
kangbaku <kangbaku@gmail.com>
2022-08-17 23:43:12 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P0, 180)
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.servoWritePin(AnalogPin.P0, 90)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P0, 131)
+})
+basic.forever(function () {
+	
+})
